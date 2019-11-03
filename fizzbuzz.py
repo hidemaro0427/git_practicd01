07cec7f56fe9bd70424a57db2ba745d46f14040f
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-03 11:46:07 +0900

    bug: SyntaxError: 'return' outside function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,9 @@
-def fizzbuzz_convert(number):
-    if number / 3 == 0:
-        return "izz"
-
-
-assert fizzbuzz_convert(3) == "Fizz"
+for number in range(1, 101):
+    if number % 15 == 0:
+        return "fizzbuzz"
+    elif number % 5 == 0:
+        return "buzz"
+    elif number % 3 == 0:
+        return "fizz"
+    else:
+        print(number)
